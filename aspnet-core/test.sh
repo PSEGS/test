e28183e0ae4b2526a852b9f44baf9e78dcca7d01
PSEGS <138562892+PSEGS@users.noreply.github.com>
2023-07-07 16:27:53 +0530

    Create test.sh

---
+++ aspnet-core/test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Name of the Docker container to force stop and remove
+CONTAINER_NAME="testcontainer"
+
+# Force stop the container using the specified name
+CONTAINER_ID=$(docker ps -aqf "name=${CONTAINER_NAME}")
+if [ -n "$CONTAINER_ID" ]; then
+    echo "Force stopping container ${CONTAINER_NAME}..."
+    docker stop force $CONTAINER_ID
+else
+    echo "Container ${CONTAINER_NAME} not found."
+fi
+
+# Remove the container using the specified name
+CONTAINER_ID=$(docker ps -aqf "name=${CONTAINER_NAME}")
+if [ -n "$CONTAINER_ID" ]; then
+    echo "Removing container ${CONTAINER_NAME}..."
+    docker rm $CONTAINER_ID
+else
+    echo "Container ${CONTAINER_NAME} not found."
+fi
+
+
+# Name of the Docker image to remove
+IMAGE_NAME="test"
+
+# Remove images with the specified name
+IMAGES=$(docker images --filter "reference=${IMAGE_NAME}" --format "{{.ID}}")
+if [ -n "$IMAGES" ]; then
+    echo "Removing images..."
+    docker rmi $IMAGES
+else
+    echo "No images found with the name ${IMAGE_NAME}."
+fi
+
+# Name for the Docker image
+IMAGE_NAME="test"
+
+# Build the Docker image
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME $DOCKERFILE_DIR
+
+echo "Docker image created successfully."
+
+
+# Name of the Docker image to create a container from
+IMAGE_NAME="test"
+
+# Name for the Docker container
+CONTAINER_NAME="testcontainer"
+
+# Create the Docker container
+echo "Creating Docker container..."
+docker run -dp 200:80 --name $CONTAINER_NAME $IMAGE_NAME
+
+echo "Docker container created successfully."
+
+exit 0
+
+
+
+echo "Done."
